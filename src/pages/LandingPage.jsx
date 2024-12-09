022362292d55932b17d08be56f21d4aa90287f90
Xander Salathe <xsalathe@gmail.com>
2024-12-09 18:48:00 +1100

    feat: landing page hero

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/Hero';
+
+export default function LandingPage() {
+    return (
+        <body>
+            <Hero />
+            {/* About */}
+            {/* How it works */}
+            {/* Features */}
+            {/* CTA */}
+            {/* FAQ */}
+            {/* Footer */}
+        </body>
+    );
+}

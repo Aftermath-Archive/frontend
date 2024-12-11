d09efc10eee2bff90097c3ca466a6752c099a719
Xander Salathe <xsalathe@gmail.com>
2024-12-11 18:49:32 +1100

    refactor: separate in app and logged out layouts

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,5 @@
-import { AppSidebar } from '@/components/Sidebar/app-sidebar';
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from '@/components/ui/sidebar';
 import Footer from '../Footer/Footer';
+import HeaderMenu from '../HeaderMenu/HeaderMenu';
 
 export default function Layout({ children }) {
     return (
@@ -21,38 +7,11 @@ export default function Layout({ children }) {
             className="flex flex-col min-h-svh mx-auto bg-background text-foreground"
             id="layout"
         >
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 h-4"
-                        />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="/dashboard">
-                                        Aftermath Archive
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                {/* <BreadcrumbSeparator className="hidden md:block" />
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                            </BreadcrumbItem> */}
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </header>
-                    <main
-                        className="flex-grow p-4 max-w-80ch"
-                        id="main-content"
-                    >
-                        {children}
-                    </main>
-                    <Footer />
-                </SidebarInset>
-            </SidebarProvider>
+            <HeaderMenu />
+            <main className="flex-grow p-4 max-w-80ch" id="main-content">
+                {children}
+            </main>
+            <Footer />
         </div>
     );
 }

fa9b10d821082ce8d870aabfc0145dd54fb5f4be
Xander Salathe <xsalathe@gmail.com>
2024-12-11 15:48:26 +1100

    refactor: group landing page components separately

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,11 @@
-import About from '@/components/About';
-import CTA from '@/components/CTA';
-import AccordionFAQ from '@/components/Faq';
-import HowItWorks from '@/components/HowItWorks';
+import About from '@/components/LandingPageComponents/About';
+import CTA from '@/components/LandingPageComponents/CTA';
+import AccordionFAQ from '@/components/LandingPageComponents/Faq';
+import HowItWorks from '@/components/LandingPageComponents/HowItWorks';
 import Footer from '@/components/Footer/Footer';
-import HeaderMenu from '@/components/HeaderMenu';
-import Hero from '@/components/Hero';
-import Features from '@/components/Features';
+import HeaderMenu from '@/components/LandingPageComponents/HeaderMenu';
+import Hero from '@/components/LandingPageComponents/Hero';
+import Features from '@/components/LandingPageComponents/Features';
 
 export default function LandingPage() {
     return (

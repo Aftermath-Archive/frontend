6ef400b3fdcae9f42b9f6fed3de11f6a03586e38
Xander Salathe <xsalathe@gmail.com>
2024-12-11 18:54:44 +1100

    feat: register page route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LandingPage from '@/pages/LandingPage';
 import DashboardPage from '@/pages/Dashboard';
 import ErrorPage from '@/pages/ErrorPage';
+import DevPage from '@/pages/Dev';
+import RegisterPage from '@/pages/RegisterPage';
 
 const appRouter = createBrowserRouter([
     {
@@ -16,6 +18,14 @@ const appRouter = createBrowserRouter([
     {
         path: '/dashboard',
         element: <DashboardPage />,
+    },
+    {
+        path: '/dev',
+        element: <DevPage />,
+    },
+    {
+        path: '/auth/register',
+        element: <RegisterPage />,
     },
     {
         path: '*', // Catch-all for unmatched routes (404)
